fd0aee16f788a9364fb21f73e1d68fa2b095c1ad
Cristhian <cristhianmartin12@gmail.com>
2019-03-06 10:03:24 -0500

    BD Modificada

---
+++ populate_social.sql
@@ -0,0 +1,14 @@
+insert into tb_social_network_company values 
+	('1', 'Facebook', 'images/facebook.png'),
+	('2', 'Instagram', 'images/instagram.png'),
+	('3', 'Twitter', 'images/twitter.png'),
+	('4', 'Youtube', 'images/youtube.png');
+    
+insert into tb_social_network values 
+	('1', 'zazu', '1', 'https://www.facebook.com/4zart/'),
+	('2', 'zazu', '2', 'http://instagram.com/_za.zu_/'),
+	('3', 'zazu', '4', 'http://youtube.com/Zicryedz'),
+	('4', '1159', '1', 'https://es-la.facebook.com/cristhianmartin.chacalcajelopez'),
+	('5', '1159', '2', 'https://www.instagram.com/cristhian1207/'),
+	('6', 'karen', '2', 'https://www.instagram.com/kuro_mello/'),
+	('7', 'that ignostic douchebag', '2', 'https://www.instagram.com/ignostic_douchebag/');
